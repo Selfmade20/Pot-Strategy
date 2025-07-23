67cbf68a4f5b51d52884894df448753d7bb79199
Selfmade20 <tebogo1686@gmail.com>
2025-07-24 01:37:29 +0200

    Creating the app layout pages and routes configuration

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,47 @@
+import "./App.css";
+import AppLayout from "./layouts/AppLayout";
+import Home from "./pages/Home";
+import Auth from "./pages/Auth";
+import Dasboard from "./pages/Dasboard";
+import Link from "./pages/Link";
+import Redirect from "./pages/Redirect";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
+
+const router = createBrowserRouter([
+  {
+    element: <AppLayout />,
+    children: [
+      {
+        path: "/",
+        element: <Home />
+      },
+      {
+        path: "/auth",
+        element: <Auth />
+      },
+      {
+        path: "/dashboard",
+        element: <Dasboard />
+      },
+      {
+        path: "/link/:id",
+        element: <Link />
+      },
+      {
+        path: "/:id",
+        element: <Redirect />
+      },
+
+    ]
+  },
+]);
+
+
 
 function App() {
   return (
-    <div >
-      <h1 className="font-bold text-6xl">Click me</h1>
-    </div>
+    <RouterProvider router={router} />
   )
 }
 
